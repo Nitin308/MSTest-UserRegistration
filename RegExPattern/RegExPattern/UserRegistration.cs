b848543450d017dbf42f751b2abc0f3848d84db8
Nitin308 <nitin3081998@gmail.com>
2022-12-15 11:26:55 +0530

    Validate Last Name

---
+++ RegExPattern/RegExPattern/UserRegistration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegExPattern
+{
+    public class UserRegistration
+    {
+        public void FirstName()
+        {
+            Console.Write("\nEnter First Name: ");
+            string firstName = Console.ReadLine();
+            var regex = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+            bool matchRes = regex.IsMatch(firstName);
+            if (matchRes == true)
+            {
+                Console.WriteLine($"{firstName} is valid");
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} is invalid. Please enter a valid First Name. First word must be capital and Name must contain minimum 3 letters.");
+                FirstName();
+            }
+        }
+
+        public void LastName()
+        {
+            Console.Write("\nEnter Last Name: ");
+            string lastName = Console.ReadLine();
+            var regex = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+            bool matchRes = regex.IsMatch(lastName);
+            if (matchRes == true)
+            {
+                Console.Write($"{lastName} is valid");
+            }
+            else
+            {
+                Console.WriteLine($"{lastName} is invalid. Please enter a valid Last Name. First word must be capital and Name must contain minimum 3 letters.");
+                LastName();
+            }
+        }
+    }
+}
