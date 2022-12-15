00ed0b79562cd293b38181ecd7e21fdeebc41044
Nitin308 <nitin3081998@gmail.com>
2022-12-15 11:05:47 +0530

    Validate First Name

---
+++ RegExPattern/RegExPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace RegExPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expressions");
+            Console.WriteLine("\n==========User Registration Form==========");
+
+            UserRegistration objUser = new UserRegistration();
+            objUser.FirstName();
+        }
+    }
+}
