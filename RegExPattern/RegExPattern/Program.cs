50498212ed21fb307f47aa65ddeea3ce832389c8
Nitin308 <nitin3081998@gmail.com>
2022-12-17 17:41:31 +0530

    Validate Phone Number

---
+++ RegExPattern/RegExPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RegExPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expressions");
+            Console.WriteLine("\n==========User Registration Form==========");
+
+            UserRegistration objUser = new UserRegistration();
+            objUser.FirstName();
+            objUser.LastName();
+            objUser.Email();
+            objUser.PhoneNumber();
+        }
+    }
+}
---
+++ RegExPattern/RegExPattern/UserRegistration.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegExPattern
+{
+    public class UserRegistration
+    {
+        public void FirstName()
+        {
+            Console.Write("\nEnter First Name: ");
+            string firstName = Console.ReadLine();
+            var regex = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+            bool matchRes = regex.IsMatch(firstName);
+            if (matchRes == true)
+            {
+                Console.WriteLine($"{firstName} is valid");
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} is invalid. Please enter a valid First Name. First word must be capital and Name must contain minimum 3 letters.");
+                FirstName();
+            }
+        }
+
+        public void LastName()
+        {
+            Console.Write("\nEnter Last Name: ");
+            string lastName = Console.ReadLine();
+            var regex = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+            bool matchRes = regex.IsMatch(lastName);
+            if (matchRes == true)
+            {
+                Console.WriteLine($"{lastName} is valid");
+            }
+            else
+            {
+                Console.WriteLine($"{lastName} is invalid. Please enter a valid Last Name. First word must be capital and Name must contain minimum 3 letters.");
+                LastName();
+            }
+        }
+
+        public void Email()
+        {
+            Console.Write("\nEnter Email (Example: abc.xyz@bl.co.in): ");
+            string email = Console.ReadLine();
+            var regex = new Regex(@"^([a][b][c])([_\.\+\-])([a-zA-Z0-9]+)\@([b][l])\.([c][o])\.([a-z]{2})*$");
+            bool matchRes = regex.IsMatch(email);
+            if (matchRes == true)
+            {
+                Console.WriteLine($"{email} is valid");
+            }
+            else
+            {
+                Console.WriteLine($"{email} is invalid. Please enter a valid email as abc.xyz@bl.co.in where xyz and in are optional");
+                Email();
+            }
+        }
+
+        public void PhoneNumber()
+        {
+            Console.Write("\nEnter Mobile Number (Example: +(country code) (10 digit number)): ");
+            string phoneNumber = Console.ReadLine();
+            var regex = new Regex(@"^\+[1-9]{1}[0-9]{0,2}\s[1-9]{1}[0-9]{9}");
+            bool matchRes = regex.IsMatch(phoneNumber);
+            if (matchRes == true)
+            {
+                Console.WriteLine($"{phoneNumber} is valid");
+            }
+            else
+            {
+                Console.WriteLine($"{phoneNumber} is invalid. Please enter a valid mobile number");
+                PhoneNumber();
+            }
+        }
+    }
+}
